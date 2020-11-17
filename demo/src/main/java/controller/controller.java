407166b3dd82e61d7d9a383636d750941859353f
vishvanthan.n <vishvanthan.n@CHNMCT335618L.ad.infosys.com>
2020-11-17 07:51:46 +0530

    second commit

--- demo/src/main/java/controller/controller.java
+++ demo/src/main/java/controller/controller.java
@@ -1,15 +1,29 @@
 package controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import service.service;
+
 @RestController
 @RequestMapping("/company")
+
+
+
 public class controller {
+	
+	@Autowired
+	private service service1;
+	
+	
 	@GetMapping("/employees")
 	public String getdata() {
-		return "Hello world";
+		String a=null;
+		return a=service1.getdata();
+		
+		
 	}
 	
 
